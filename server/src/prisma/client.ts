85977619503538f6b0a6f8c846ade6f66daa3b38
Habtamu Abd <habtamubefekadu20@gmail.com>
2026-04-13 12:13:26 +0300

    update changes in server

--- server/src/prisma/client.ts
+++ server/src/prisma/client.ts
@@ -1,12 +1,17 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { env } from "../config/env";
 
 const globalForPrisma = globalThis as typeof globalThis & {
   prisma?: PrismaClient;
 };
 
+const adapter = new PrismaPg(env.databaseUrl);
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: ["warn", "error"]
   });
 
