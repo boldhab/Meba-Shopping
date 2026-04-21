1b70deaad315cc4d257713a7cf902d27d6d9e488
Habtamu Abd <habtamubefekadu20@gmail.com>
2026-04-21 21:42:50 +0300

    update changes in client

--- client/components/layout/Sidebar.tsx
+++ client/components/layout/Sidebar.tsx
@@ -8,6 +8,7 @@ const links = [
   { href: "/admin/products", label: "Products" },
   { href: "/admin/deals", label: "Deals" },
   { href: "/admin/orders", label: "Orders" },
+  { href: "/admin/users", label: "Users" },
   { href: "/admin/settings", label: "Settings" },
 ];
 
